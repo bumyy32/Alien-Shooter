acd9a931fc0c1b2d10987dcbc7cb472c8c90c2d0
bumyy <bumyy32@gmail.com>
2022-12-07 19:09:58 -0300

    aliens!

---
+++ codes/aliens.py
@@ -0,0 +1,8 @@
+import pygame
+
+class Alien(pygame.sprite.Sprite):
+    def __int__(self, color, x,y):
+        super().__init__()
+        file_path = '../graphics/enemies/' + color + '.png'
+        self.image = pygame.image.load(file_path).convert_alpha()
+        self.rect = self.image.get_rect(topleft = (x,y))
